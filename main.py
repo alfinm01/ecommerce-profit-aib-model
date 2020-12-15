4b63b0fedc7f5bac1cb262b7bca21a75de71a19a
Alfian Maulana <alfianm.ibrahim@gmail.com>
2020-12-15 21:55:15 +0700

    Backend done

---
+++ main.py
@@ -0,0 +1,35 @@
+from flask import Flask, request, jsonify, render_template, redirect
+import pickle
+import numpy as np
+
+app = Flask(__name__)
+model = pickle.load(open('mlr_model.pkl', 'rb'))
+
+# constants (in usd)
+sales_max = 250.0
+shipping_cost_max = 16.8
+profit_max = 167.5
+
+@app.route("/", methods=['POST', 'GET'])
+def home():
+    if request.method == 'GET':
+        return 'OK'
+    else:
+        sales = request.json['sales'] / sales_max
+        shipping_cost = request.json['shipping_cost'] / shipping_cost_max
+
+        features = [
+                    sales, shipping_cost, 
+                    request.json['auto_and_accessories'], request.json['electronic'], request.json['fashion'], request.json['home_and_furniture'],
+                    request.json['order_priority-critical'], request.json['order_priority-high'], request.json['order_priority-medium'], request.json['order_priority-low']
+                   ]
+        final_features = [np.array(features)]
+        result = model.predict(final_features)
+
+        # in usd
+        profit = result[0][0] * profit_max
+
+        return jsonify({ 'profit': profit })
+
+if __name__ == '__main__':
+    app.run(debug=True)
