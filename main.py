083003ab7e5f2aa8be1b01c4567b5299643cecd2
Alfian Maulana <alfianm.ibrahim@gmail.com>
2020-12-17 20:35:24 +0700

    Allow CORS

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from flask import Flask, request, jsonify, render_template, redirect
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 import pickle
 import numpy as np
 
 app = Flask(__name__)
+CORS(app)
 model = pickle.load(open('mlr_model.pkl', 'rb'))
 
 # constants (in usd)
